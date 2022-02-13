7f9abe97993a768833cc5069ba6efc316b20a6b3
Luan Sá <luan.augusto.sa@gmail.com>
2022-02-12 23:23:39 -0300

    5.2 finalizado

--- bloco-05-DOM/dia-02-Trabalhando-com-elementos/exerc02-vamos-praticar/script.js
+++ bloco-05-DOM/dia-02-Trabalhando-com-elementos/exerc02-vamos-praticar/script.js
@@ -89,3 +89,57 @@ newTagEx07.className = "small-image left-img";
 // newTagEx07.innerHTML =  "<img src="https://www.w3schools.com/images/lamp.jpg" >" ;
 whereLeftContent.appendChild(newTagEx07);
 
+//q08 Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
+whereRightContent = document.querySelector(".right-content");
+const newTagEx08 = document.createElement('ul');
+whereRightContent.appendChild(newTagEx08);
+let arrayNumbers = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis',
+  'Sete', 'Oito', 'Nove', 'Dez'];
+for (let key in arrayNumbers) {
+  const newTagEx088 = document.createElement('li');
+  newTagEx088.innerHTML = arrayNumbers[key];
+  newTagEx08.appendChild(newTagEx088);
+}
+
+
+// 9. Adicione 3 tags `h3`, todas sendo filhas do `main` criado no passo 2.
+whereMainTeste = document.querySelector("main");
+whereMainTeste.className = "main-content";
+for (let i = 1; i <= 3; i += 1) {
+  const newTagEx09 = document.createElement("h3");
+  newTagEx09.innerHTML = "Show " + i;
+  whereMainTeste.appendChild(newTagEx09);
+}
+
+
+ // PARTE 2
+
+      // 1.Adicione a classe title na tag h1 criada;
+      const title = document.querySelector("h1");
+      title.className = "title";
+
+      //  // 2.Adicione a classe description nas 3 tags h3 criadas;
+      //  const elementsH3 = document.getElementsByTagName('h3');
+      //  for (let index = 0; index < 3; index += 1) {
+      //    elementsH3[index].className = 'description';
+      //  }
+
+      //  // 3. Remova a `section` criado no passo 5 (aquele que possui a classe `left-content`). Utilize a função `.removeChild()`;
+      // const sectionLeftContent = document.getElementsByClassName('left-content')[0];
+      // elementMain.removeChild(sectionLeftContent);
+
+
+
+      // // 4. Centralize a `section` criado no passo 6 (aquele que possui a classe `right-content`). Dica: para centralizar, basta configurar o `margin-right: auto` da `section`;
+      // const sectionRightContent = document.getElementsByClassName('right-content')[0];
+      // newTagEx06.style.marginRight='auto';
+
+      // // 5. Troque a cor de fundo do elemento pai da `section` criada no passo 3 (aquela que possui a classe `center-content`) para a cor verde;
+      // const sectionCenterContent = document.getElementsByClassName('center-content')[0];
+      // sectionCenterContent.parentNode.style.backgroundColor = 'green';
+
+      //   // 6.Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+      //   ul.lastChild.remove();
+      //   ul.lastChild.remove();
+
+       
